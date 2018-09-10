3fa32a2c828f40b56eb20e3f8087d9e4ae48d6ef
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-10 10:16:29 -0400

    Active Directory Login Using C#
    On this tutorial we will show how to authenticate a user on your C# .Net application using Active Directory credentials.

---
+++ ActiveDirectoryAuth/ActiveDirectoryAuth/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveDirectoryAuth
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [DllImport("advapi32.dll")]
+        public static extern Boolean LogonUser(string name, string domain, string pass, int logType, int logpv, ref IntPtr pht);
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text.Length == 0)
+            {
+                MessageBox.Show("Please Enter Username");
+                return;
+            }
+
+            if (txtPassword.Text.Length == 0)
+            {
+                MessageBox.Show("Please Enter Password");
+                return;
+            }
+            IntPtr th = IntPtr.Zero;
+            Boolean is_user_auth = LogonUser(txtUserName.Text, "HCA", txtPassword.Text, 2, 0,  ref th);
+
+
+            if (is_user_auth)
+                MessageBox.Show("User Authorized");
+            else
+                MessageBox.Show("Access Denied");
+
+        }
+    }
+}
